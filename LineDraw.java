8fd8013cffdbda4a89e3d2500c358964369d2979
mshimura15 <mshimura15@matts-mbp.punahou.edu>
2015-03-12 11:53:44 -1000

    First Commit
    Adding Code to GitHub

---
+++ LineDraw.java
@@ -0,0 +1,124 @@
+public class LineDraw
+{
+  public static void main (String[] args)
+  {
+    System.out.println();
+    
+    System.out.println("               " + "  " + "0 " + "1 " + "2 " + "3 ");
+    
+    System.out.println("               " + "0 " + ". " + "._" + "._" + ". ");
+    
+    System.out.println("               " + "1 " + ". " + "|_" + "|_" + "| ");
+    
+    System.out.println("               " + "2 " + ". " + ". " + ". " + ". ");
+    
+    System.out.println("               " + "3 " + ". " + ". " + ". " + ". ");
+    
+    System.out.println();
+    
+    System.out.println();
+    
+    System.out.println("               " + "  " + "0 " + "1 " + "2 " + "3 ");
+    
+    System.out.println("               " + "0 " + ". " + ". " + ". " + ". ");
+    
+    System.out.println("               " + "1 " + ". " + ". " + ". " + ". ");
+    
+    System.out.println("               " + "2 " + ". " + ". " + ". " + ". ");
+    
+    System.out.println("               " + "3 " + ". " + ". " + ". " + ". ");
+    
+    System.out.println();
+    
+    
+    
+    
+    System.out.println();
+    
+    System.out.println("               " + "  " + "0   " + "1   " + "2   " + "3   " + "4   " + "5   " + "6   " + "7   " + "8   " + "9   " );
+    
+    System.out.println("               " + "0 " + ".___" + ".___" + ".___" + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    
+    System.out.println("               " + "  " + "| A " + "| A " + "| A " + "|   " + "    " + "    " + "    " + "    " + "    " + "   " );
+    
+    System.out.println("               " + "1 " + ".___" + ".___" + ".___" + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    
+    System.out.println("               " + "  " + "| B " + "| B " + "| A " + "|   " + "    " + "    " + "    " + "    " + "    " + "   " );
+    
+    System.out.println("               " + "2 " + ".___" + ".___" + ".___" + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "3 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "4 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "5 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "6 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "7 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "8 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "9 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    
+    System.out.println();    
+    System.out.println("               " + "  " + "0   " + "1   " + "2   " + "3   " + "4   " + "5   " + "6   " + "7   " + "8   " + "9   " );
+    
+    System.out.println("               " + "0 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "1 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "2 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "3 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "4 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "5 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "6 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "7 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "8 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    
+    System.out.println("               " + "9 " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " + ".   " );
+    System.out.println();
+    //print player 1 and player 2 score
+    //print player turn
+    
+    String[][] t = { 
+      { "  ", "0   ", "1   ", "2   ", "3   ", "4   ", "5   ", "6   ", "7   ", "8   ", "9   " },  
+      { "0 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   " },  
+      { "1 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   " },
+      { "2 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "3 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "4 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "5 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "6 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "7 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "8 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  },
+      { "9 ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   ", ".   "  }
+    }; 
+    
+    System.out.println(t);
+  }
+}
